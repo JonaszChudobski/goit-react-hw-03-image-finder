9f73f4e54987da25f605a3ccad352270b0b73e3b
JonaszChudobski <jonasz.chudobski@wp.pl>
2023-06-26 20:50:18 +0200

    task done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.module.css';
 import { Modal } from 'components/Modal/Modal';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -7,108 +7,92 @@ import { Loader } from './Loader/Loader';
 import { Button } from './Button/Button';
 import { handleFetch } from 'api/fetchFunction';
 
-const INITIAL_STATE = {
-  srclarge: '',
-  alt: '',
-};
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [isButton, setIsButton] = useState(null);
+  const [error, setError] = useState(null);
+  const [srclarge, setSrclarge] = useState('');
+  const [alt, setAlt] = useState('');
 
-export class App extends Component {
-  state = {
-    images: [],
-    query: '',
-    isLoading: false,
-    page: 1,
-    isButton: null,
-    error: null,
-    srclarge: '',
-    alt: '',
+  const handlePagesAmount = () => {
+    setPage(page + 1);
   };
 
-  handlePagesAmount = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
     const form = e.currentTarget;
     const input = form.elements[1].value;
-    this.setState({
-      query: input,
-      page: 1,
-      isButton: null,
-    });
+    setQuery(input);
+    setPage(1);
+    setIsButton(null);
   };
 
-  handleGet = async () => {
-    this.setState({ isLoading: true });
-    const { query, page } = this.state;
-    try {
-      const response = await handleFetch(query, page);
-      if (page === 1) {
-        this.setState({
-          images: response.data.hits,
-          isButton: response.data.totalHits,
-        });
-      } else {
-        this.setState(prevState => ({
-          images: [...prevState.images, ...response.data.hits],
-        }));
+  const handleGet = async () => {
+    if (query === '') {
+      return null;
+    } else {
+      setIsLoading(true);
+      try {
+        const response = await handleFetch(query, page);
+        if (page === 1) {
+          setImages(response.data.hits);
+          setIsButton(response.data.totalHits);
+        } else {
+          setImages([...images, ...response.data.hits]);
+        }
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
       }
-    } catch (error) {
-      this.setState({ error });
-    } finally {
-      this.setState({ isLoading: false });
     }
   };
 
-  onClick = e => {
+  const onClick = e => {
     const src = e.currentTarget.dataset.srclarge;
     const alt = e.currentTarget.getAttribute('alt');
-    this.setState({
-      srclarge: src,
-      alt: alt,
-    });
+    setSrclarge(src);
+    setAlt(alt);
   };
 
-  onExitClick = () => {
-    this.setState({ ...INITIAL_STATE });
+  const onExitClick = () => {
+    setSrclarge('');
+    setAlt('');
   };
 
-  onExitEscape = e => {
-    if (e.keyCode === 27) this.setState({ ...INITIAL_STATE });
-  };
-
-  render() {
-    const { images, page, isLoading, isButton, srclarge, alt } = this.state;
-    return (
-      <>
-        <Searchbar onSubmit={this.handleSubmit} />
-        {isLoading ? (
-          <Loader />
-        ) : (
-          <ImageGallery images={images} onClick={this.onClick} />
-        )}
-        {!isLoading && isButton > 12 && page < Math.ceil(isButton / 12) ? (
-          <Button onClick={this.handlePagesAmount} />
-        ) : null}
-        {srclarge ? (
-          <Modal
-            src={srclarge}
-            alt={alt}
-            onExitClick={this.onExitClick}
-            onExitEscape={this.onExitEscape}
-          />
-        ) : null}
-      </>
-    );
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.page !== this.state.page ||
-      prevState.query !== this.state.query
-    ) {
-      this.handleGet();
+  const onExitEscape = e => {
+    if (e.keyCode === 27) {
+      setSrclarge('');
+      setAlt('');
     }
-  }
-}
+  };
+
+  useEffect(() => {
+    handleGet();
+  }, [query, page]);
+
+  return (
+    <>
+      <Searchbar onSubmit={handleSubmit} />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <ImageGallery images={images} onClick={onClick} />
+      )}
+      {!isLoading && isButton > 12 && page < Math.ceil(isButton / 12) ? (
+        <Button onClick={handlePagesAmount} />
+      ) : null}
+      {srclarge ? (
+        <Modal
+          src={srclarge}
+          alt={alt}
+          onExitClick={onExitClick}
+          onExitEscape={onExitEscape}
+        />
+      ) : null}
+    </>
+  );
+};
